2d17d1b39e58e98d967878aae821ffd1b8baa23e
Dobromir Hristov <djdobro@abv.bg>
2017-06-21 21:58:10 +0300

    refactor($vue): Some more code to the Login with vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,12 @@
 <template>
 	<div>
-		some template
+		<form @submit.prevent="submitForm">
+			<label>Name</label>
+			<input type="text" name="name" v-model='query.name'>
+			<label>Password</label>
+			<input type="password" name="password" v-model='query.password'>
+			<button>Submit</button>
+		</form>
 	</div>
 </template>
 
@@ -8,7 +14,18 @@
 	export default {
 		data () {
 			return {
-				data:'some data'
+				data:'some data',
+				query: {
+					name: '',
+					password: ''
+				}
+			}
+		},
+		methods: {
+			submitForm (e) {
+				this.$http.post('some-url', this.query).then((r) => {
+					this.$notify(r.body.message)
+				})
 			}
 		}
 	}
