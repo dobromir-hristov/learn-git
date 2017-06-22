75556a0c20ebcacdcbb1fd3ec4be975f2e9198c1
Dobromir Hristov <djdobro@abv.bg>
2017-06-23 00:02:20 +0300

    feat(vue): Added new Register component

---
+++ src/components/register.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<form @submit.prevent="submitForm">
+			<label>Name</label>
+			<input type="text" name="name" v-model='query.name'>
+			<label>Password</label>
+			<input type="password" name="password" v-model='query.password'>
+			<button>Submit</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				data:'some data',
+				query: {
+					name: '',
+					password: ''
+				}
+			}
+		},
+		methods: {
+			submitForm (e) {
+				this.$http.post('some-url', this.query).then((r) => {
+					this.$notify(r.body.message)
+					location.href = r.body.redirectTo
+				}, r => {
+					this.$notify(r.body.message, 'error')
+				})
+			}
+		}
+	}
+</script>
